c674b313a89a9fb755d1b3d91b08c00ebadd5951
Temur1221 <tem67904@gmail.com>
2026-09-03 23:36:38 +0400

    Add AccountIntro page

---
+++ src/pages/AccountIntro.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+import AccountHeroSection from '../AccountHeroSection';
+import AccountBranchNotice from '../AccountBranchNotice';
+import AccountDocuments from '../AccountDocuments';
+import AccountAccordionSection from '../AccountAccordionSection';
+import EvocaBanner from '../EvocaBanner';
+import AccountSubNav from '../AccountSubNav';
+
+export default function AccountIntro() {
+  return (
+    <>
+      <Header />
+      <AccountSubNav/>
+      <AccountHeroSection />
+      <AccountBranchNotice />
+      <AccountAccordionSection/>
+      <AccountDocuments/>
+      <EvocaBanner/>
+      <Footer />
+    </>
+  );
+}
