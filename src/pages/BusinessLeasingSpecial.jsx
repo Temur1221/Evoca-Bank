e88ec38492273512e0cc64444a6847f4b579699f
Temur1221 <tem67904@gmail.com>
2026-09-18 18:07:44 +0400

    Ավելացվել է BusinessLeasingSpecial.jsx էջը

---
+++ src/pages/BusinessLeasingSpecial.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+import BusinesSubNav from '../BusinesSubNav';
+import SpecialOfferSection from '../SpecialOfferSection'; // Ներմուծում ենք նոր ֆայլը
+
+export default function BusinessLeasingSpecial() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <BusinesSubNav />
+
+      <main className="flex-grow w-full">
+        <SpecialOfferSection />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
