bcf9daca851c67a7720cb6f78b39a381545868e1
Temur1221 <tem67904@gmail.com>
2026-09-18 18:13:12 +0400

    Ավելացվել է EvocasalaryMain.jsx էջը

---
+++ src/pages/EvocasalaryMain.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import EvocasalaryHero from '../EvocasalaryHero'
+import CapitalNewsSection from '../CapitalNewsSection'
+import SalaryDetails from '../SalaryDetails'
+import SalaryFAQ from '../SalaryFAQ'
+
+export default function EvocasalaryMain() {
+  return (
+    <>
+    <Header/>
+    <EvocasalaryHero/>
+    <SalaryDetails/>
+    <CapitalNewsSection />
+    <SalaryFAQ/>
+    <Footer/>
+    </>
+)
+}
