c7e967e1807d461696d89a1595719cc41ddf1488
Temur1221 <tem67904@gmail.com>
2026-09-18 18:06:13 +0400

    Ավելացվել է BusinessDeposits.jsx էջը

---
+++ src/pages/BusinessDeposits.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../Header'
+import EvocaBanner from '../EvocaBanner'
+import Footer from '../Footer'
+import BusinessAccountIntroSection from '../BusinessAccountIntroSection'
+import BusinessDepositsDetailsSection from '../BusinessDepositsDetailsSection'
+import BusinessDepositsAccordion from '../BusinessDepositsAccordion'
+import BusinessSubHeader from '../BusinessSubHeader'
+
+export default function BusinessDeposits() {
+  return (
+    <>
+    <Header/>
+    <BusinessSubHeader/>
+    <BusinessAccountIntroSection />
+    <BusinessDepositsDetailsSection/>
+    <BusinessDepositsAccordion/>
+    <EvocaBanner/>
+    <Footer/>
+    </>
+)
+}
