9b62c52a4e271e71b1ff2f0957734df4f1367f92
Temur1221 <tem67904@gmail.com>
2026-09-18 16:29:02 +0400

    Թարմացվել է App.jsx ֆայլը

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,110 @@ import Qarter from "./pages/Qarter";
 import CardsList from "./CardsList";
 import CardTerms from "./CardTerms";
 import PensionCards from "./pages/PensionCards";
-import EvocaBenefits from "./pages/EvocaBenefits";
+import EvocaBenefits from "./SalaryDetails";
 import Deposits from "./pages/Deposits";
 import Partners from "./pages/Partners";
+import ImportantInfo from "./pages/ImportantInfo";
+import AccountIntro from "./pages/AccountIntro";
+import MetalAccountsIntro from "./pages/MetalAccountsIntro";
+import NonResidentRemote from "./pages/NonResidentRemote";
+import SafeDepositBoxes from "./pages/SafeDepositBoxes";
+import MoneyTransfers from "./pages/MoneyTransfers";
+import PaymentSystemsPage from "./pages/PaymentSystemsPage";
+import SecuritiesPage from "./pages/SecuritiesPage";
+import BondsSection from "./pages/BondsSection";
+import HkdMainPage from "./pages/HkdMainPage";
+import RepoMainPage from "./pages/RepoMainPage";
+import EvocainvestMainPage from "./pages/EvocainvestMainPage";
+import EvocasalaryMain from "./pages/EvocasalaryMain";
+import EvocaTouch from "./pages/EvocaTouch";
+
+// Տերմինալի տեղադրման հայտի էջը
+import TerminalRequestForm from "./pages/TerminalRequestForm";
+
+// Mobile POS էջը (ուղղված ճանապարհը դեպի pages պապկա)
+import MobilePOSPage from "./pages/MobilePOSPage";
+
+// Բիզնես բաժնի էջեր
+import BusinessLoans from "./pages/BusinessLoans";
+import BusinessLeasing from "./pages/BusinessLeasing";
+import BusinessLeasingSpecial from "./pages/BusinessLeasingSpecial";
+import BusinessAccounts from "./pages/BusinessAccounts";
+import BusinessDeposits from "./pages/BusinessDeposits"; 
+import BusinessMetalAccounts from "./pages/BusinessMetalAccounts"; 
+import BusinessSecurities from "./pages/BusinessSecurities";
+import BusinessTrade from "./pages/BusinessTrade"; 
+import BusinessFactoring from "./pages/BusinessFactoring"; 
+import AccreditiveContent from "./pages/AccreditiveContent";
+import BusinessInkasso from "./pages/BusinessInkasso";
+import BusinessVPos from "./pages/BusinessVPos";
+import BusinessPos from "./pages/BusinessPos"; 
+import BusinessTariffs from "./pages/BusinessTariffs";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Գլխավոր էջ */}
         <Route path="/" element={<Home />} />
         <Route path="/partners" element={<Partners />} />
-        {/* Անհատական էջ */}
         <Route path="/anhat/*" element={<Anhat />} />
-
-        {/* Մյուս էջերը */}
         <Route path="/loans" element={<Anhat />} />
-        
-        {/* Ահա այստեղ /deposits-ը միացված է Deposits բաղադրիչին */}
         <Route path="/deposits" element={<Deposits />} /> 
-
-        <Route path="/accounts" element={<Anhat />} />
-        <Route path="/transfers" element={<Anhat />} />
-        <Route path="/qarter" element={<Qarter />} />
+        <Route path="/important-info" element={<ImportantInfo />} />
+        <Route path="/accounts" element={<AccountIntro />} />
+        <Route path="/unallocated-metal-accounts" element={<MetalAccountsIntro />} />
+        <Route path="/non-resident-remote" element={<NonResidentRemote />} />
+        <Route path="/safe-deposit-boxes" element={<SafeDepositBoxes />} />
+        <Route path="/money-transfers" element={<MoneyTransfers />} />
+        <Route path="/payment-systems" element={<PaymentSystemsPage />} />
         
-        {/* Քարտերի հիմնական ցանկ */}
+        {/* Տերմինալի տեղադրման հայտի ուղին */}
+        <Route path="/terminal-request" element={<TerminalRequestForm />} />
+
+        {/* Արժեթղթերի էջերը */}
+        <Route path="/securities" element={<SecuritiesPage />} />
+        <Route path="/securities/services" element={<SecuritiesPage />} />
+        <Route path="/securities/bonds" element={<BondsSection />} />
+        <Route path="/securities/hcd" element={<HkdMainPage />} />
+        <Route path="/securities/repo" element={<RepoMainPage />} />
+        <Route path="/securities/evocainvest" element={<EvocainvestMainPage />} />
+
+        {/* Բիզնես բաժնի էջերը */}
+        <Route path="/biznes/loans" element={<BusinessLoans />} />
+        <Route path="/biznes/leasing" element={<BusinessLeasing />} />
+        <Route path="/biznes/leasing/special" element={<BusinessLeasingSpecial />} />
+        <Route path="/biznes/accounts" element={<BusinessAccounts />} />
+        <Route path="/biznes/deposits" element={<BusinessDeposits />} />
+        <Route path="/biznes/metal-accounts" element={<BusinessMetalAccounts />} />
+        <Route path="/biznes/securities" element={<BusinessSecurities />} />
+        
+        {/* Առևտրի ֆինանսավորման և ֆակտորինգի էջերը */}
+        <Route path="/biznes/trade" element={<BusinessTrade />} />
+        <Route path="/biznes/factoring" element={<BusinessFactoring />} />
+        <Route path="/biznes/trade/letter-of-credit" element={<AccreditiveContent />} />
+        <Route path="/biznes/trade/inkasso" element={<BusinessInkasso />} />
+        
+        {/* Դիջիթալ բաժնի ենթաէջեր */}
+        <Route path="/biznes/trade/digital/v-pos" element={<BusinessVPos />} />
+        <Route path="/biznes/trade/digital/pos" element={<BusinessPos />} />
+        <Route path="/biznes/trade/digital/tariffs" element={<BusinessTariffs />} />
+        
+        {/* Mobile POS / mPOS էջը */}
+        <Route path="/biznes/trade/digital/mpos" element={<MobilePOSPage />} />
+
+        {/* EvocaSALARY էջը */}
+        <Route path="/salary" element={<EvocasalaryMain />} />
+
+        {/* EvocaTOUCH էջը */}
+        <Route path="/touch" element={<EvocaTouch />} />
+
+        <Route path="/transfers" element={<Navigate to="/money-transfers" replace />} />
+        <Route path="/qarter" element={<Qarter />} />
         <Route path="/cards" element={<CardsList />} />
         <Route path="/benefits" element={<EvocaBenefits />} />
-        {/* Քարտերի պայմանների էջը */}
         <Route path="/terms" element={<CardTerms />} />
-
-        {/* Սոցիալական/կենսաթոշակային քարտերի էջը */}
         <Route path="/social-cards" element={<PensionCards />} />
-
-        {/* Ցանկացած անհայտ URL-ի դեպքում տանում է /anhat */}
+        
         <Route path="*" element={<Navigate to="/anhat" replace />} />
       </Routes>
     </BrowserRouter>
