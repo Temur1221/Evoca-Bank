ca5ac87e6d12ccf382e09cc7e12c09918c2351da
Temur1221 <tem67904@gmail.com>
2026-09-18 18:16:42 +0400

    Ավելացվել է SecuritiesPage.jsx էջը

---
+++ src/pages/SecuritiesPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+import SecuritiesSubNav from '../SecuritiesSubNav';
+import InvestmentServices from '../InvestmentServices';
+import InvestmentDetails from '../InvestmentDetails';
+import InvestmentAccordion from '../InvestmentAccordion';
+import CapitalNewsSection from '../CapitalNewsSection';
+
+export default function SecuritiesPage() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col justify-between">
+      <div>
+        <Header />
+        <SecuritiesSubNav />
+        <main className="max-w-7xl mx-auto px-4 sm:px-8 lg:px-16 py-8 space-y-8">
+          <InvestmentServices />
+          <InvestmentDetails />
+          <InvestmentAccordion />
+          <CapitalNewsSection />
+        </main>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
