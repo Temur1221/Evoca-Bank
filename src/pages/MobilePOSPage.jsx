291dbd663b534dd7d3d550d63b4ec7c02cdfc7b5
Temur1221 <tem67904@gmail.com>
2026-09-18 18:17:45 +0400

    Ավելացվել է MobilePOSPage.jsx էջը

---
+++ src/pages/MobilePOSPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import MobilePOSContent from '../MobilePOSContent'
+import MPOSContent from '../MPOSContent'
+
+export default function MobilePOSPage() {
+  return (
+    <>
+      <Header/>
+      <MobilePOSContent/>
+      <MPOSContent/>
+      <Footer/>  
+    </>
+)
+}
