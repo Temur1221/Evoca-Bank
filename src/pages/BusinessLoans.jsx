b5fea43f752fa9e313bec0a8aad45545ada96d33
Temur1221 <tem67904@gmail.com>
2026-09-18 18:08:40 +0400

    Ավելացվել է BusinessLoans.jsx էջը

---
+++ src/pages/BusinessLoans.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import BusinessLoansList from '../BusinessLoansList'
+
+export default function BusinessLoans() {
+  return (
+    <>
+        <Header/>
+        <BusinessLoansList/>
+        <Footer/>
+    </>
+  )
+}
