667da4406b85b2ccfce72d50810713b05f8b558f
Temur1221 <tem67904@gmail.com>
2026-09-18 19:11:54 +0400

    Add BusinessMoneyTransfers page

---
+++ src/pages/BusinessMoneyTransfers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../Footer'
+import Header from '../Header'
+import EvocaBanner from '../EvocaBanner'
+import BusinessTransferSection from '../BusinessTransferSection'
+import GeneralTerms from '../GeneralTerms'
+export default function BusinessMoneyTransfers() {
+  return (
+    <>
+        <Header/>
+        <BusinessTransferSection/>
+        <GeneralTerms/>
+        <EvocaBanner/>
+        <Footer/>
+    </>
+)
+}
