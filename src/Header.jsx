cbb0a70503d9b9fe56970fd6d5c5fb7f276790c6
Temur1221 <tem67904@gmail.com>
2026-09-18 16:31:00 +0400

    Թարմացվել է Header.jsx ֆայլը

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNav from './TopNav';
 import MainNav from './MainNav';
 
 export default function Header() {
+  // Սահմանում ենք, թե որ բաժինն է սկզբում ակտիվ (օրինակ՝ 'anhat')
+  const [activeTab, setActiveTab] = useState('anhat');
+
   return (
-    <header className="w-full font-sans border-b border-gray-200">
-      <TopNav />
-      <MainNav />
+    <header className="w-full shadow-md bg-white">
+      {/* Փոխանցում ենք activeTab-ը և setActiveTab-ը TopNav-ին */}
+      <TopNav activeTab={activeTab} onSelectTab={setActiveTab} />
+
+      {/* Փոխանցում ենք activeTab-ը MainNav-ին, որպեսզի իմանա ինչ տեքստեր ցույց տա */}
+      <MainNav activeTab={activeTab} />
     </header>
   );
 }
