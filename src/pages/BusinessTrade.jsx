832aba2dd12614e20f697b7cf1a134f00dfc7093
Temur1221 <tem67904@gmail.com>
2026-09-18 18:10:20 +0400

    Ավելացվել է BusinessTrade.jsx էջը

---
+++ src/pages/BusinessTrade.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+import BusinessTradeSubHeader from '../BusinessTradeSubHeader';
+import CapitalNewsSection from '../CapitalNewsSection';
+import BusinessTradeGuarantee from '../BusinessTradeGuarantee';
+import BusinessTradeAccordion from '../BusinessTradeAccordion';
+
+export default function BusinessTrade() {
+  const [activeTab] = useState('biznes');
+
+  return (
+    <>
+        <Header/>
+        <BusinessTradeSubHeader/>
+        <BusinessTradeGuarantee/>
+        <BusinessTradeAccordion/>
+        <CapitalNewsSection/>
+        <Footer/>
+    </>
+  );
+}
