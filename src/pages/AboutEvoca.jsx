28cee479af96445f37c71a4fd56c5373120a167d
Temur1221 <tem67904@gmail.com>
2026-09-19 15:20:25 +0400

    Add AboutEvoca page

---
+++ src/pages/AboutEvoca.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import AboutSubHeader from '../AboutSubHeader'
+import AboutGeneral from '../AboutGeneral'
+import CorporateSocialResponsibility from '../CorporateSocialResponsibility'
+import BankBrandIdentity from '../BankBrandIdentity'
+
+export default function AboutEvoca() {
+  return (
+    <>
+        <Header/>
+        <AboutSubHeader/>
+        <AboutGeneral/>
+        <CorporateSocialResponsibility/>
+        <BankBrandIdentity/>
+        <Footer/>
+    </>
+  )
+}
