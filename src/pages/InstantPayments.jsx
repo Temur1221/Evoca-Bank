8ecbf5dbfa6bb245eb4e67381080447793edc258
Temur1221 <tem67904@gmail.com>
2026-09-19 15:19:51 +0400

    Add InstantPayments page

---
+++ src/pages/InstantPayments.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InstantPaymentHeader from '../InstantPaymentHeader'
+import MainCategories from '../MainCategories'
+import SiteFooter from '../SiteFooter'
+
+export default function InstantPayments() {
+  return (
+    <>
+        <InstantPaymentHeader/>
+        <MainCategories/>
+        <SiteFooter/>
+    </>
+)
+}
