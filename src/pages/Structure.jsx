11a24db5cebfb92bd96ca954ed64e36c9c3a4446
Temur1221 <tem67904@gmail.com>
2026-09-19 15:18:44 +0400

    Add Structure page

---
+++ src/pages/Structure.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header'
+import AboutSubHeader from '../AboutSubHeader'
+import Footer from '../Footer'
+import BankStructure from '../BankStructure'
+
+export default function Structure() {
+  return (
+    <>
+      <Header/>
+      <AboutSubHeader/>
+      <BankStructure/>
+      <Footer/>
+    </>
+)
+}
