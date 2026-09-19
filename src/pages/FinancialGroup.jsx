1dac078ad3f0cbc0323e6954c50150f884a26dbc
Temur1221 <tem67904@gmail.com>
2026-09-19 15:18:24 +0400

    Add Financial Group page

---
+++ src/pages/FinancialGroup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import AboutSubHeader from '../AboutSubHeader'
+import FinancialGroupSection from '../FinancialGroupSection'
+
+export default function FinancialGroup() {
+  return (
+     <>
+        <Header/>
+        <AboutSubHeader/>
+        <FinancialGroupSection/>
+        <Footer/>
+    </>
+)
+}
