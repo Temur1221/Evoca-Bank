441f218aca712484438fb102657fabfa66625d5c
Temur1221 <tem67904@gmail.com>
2026-09-19 15:18:50 +0400

    Add BankStructure component

---
+++ src/BankStructure.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function BankStructure() {
+  return (
+      <>
+      <div className='flex justify-center'>
+      <div className="text-center space-y-3">
+        <h2 className="text-2xl sm:text-3xl font-bold text-gray-900">
+            Բանկի կառուցվածքը
+          </h2>
+          <img className='w-[800px]' src="https://www.evoca.am/file_manager/structure/Organizational%20Structure-arm.png" alt="" />
+      </div>
+      </div>
+    </>
+)
+}
