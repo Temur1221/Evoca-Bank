4f338d6810cae86433917ecc437cd40f87ed2d77
Temur1221 <tem67904@gmail.com>
2026-09-19 15:19:02 +0400

    Add Management page

---
+++ src/pages/Management.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header';
+import AboutSubHeader from '../AboutSubHeader';
+import Footer from '../Footer';
+import ManagementSection from '../ManagementSection';
+
+export default function Management() {
+  return (
+    <>
+      <Header />
+      <AboutSubHeader />
+      <main className="flex-grow max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6 sm:p-10">
+          <ManagementSection />
+        </div>
+      </main>
+      <Footer/>
+    </>
+  )
+}
