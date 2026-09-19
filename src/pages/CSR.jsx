6b5265de1b2bf567366b976bf9755875c2f3bb24
Temur1221 <tem67904@gmail.com>
2026-09-19 15:14:16 +0400

    Add CSR page and components

---
+++ src/pages/CSR.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import AboutSubHeader from '../AboutSubHeader'
+import CSRHero from '../CSRHero'
+import CapitalNewsSection from '../CapitalNewsSection'
+import CSRDescription from '../CSRDescription'
+
+export default function CSR() {
+  return (
+    <>
+        <Header/>
+        <AboutSubHeader/>
+        <CSRHero/>
+        <CSRDescription/>
+        <CapitalNewsSection/>
+        <Footer/>
+    </>
+)
+}
