2d797dbbecff7331bb552492ea296dd8c16f6548
Temur1221 <tem67904@gmail.com>
2026-09-25 20:55:51 +0400

    Update BusinessMetalAccounts page

--- src/pages/BusinessMetalAccounts.jsx
+++ src/pages/BusinessMetalAccounts.jsx
@@ -4,6 +4,7 @@ import Footer from '../Footer'
 import ReviewsSwiper from '../ReviewsSwiper'
 import BusinessMetalAccountsHero from '../BusinessMetalAccountsHero'
 import BusinessMetalAccountsDetails from '../BusinessMetalAccountsDetails'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function BusinessMetalAccounts() {
   return (
@@ -12,6 +13,7 @@ export default function BusinessMetalAccounts() {
     <BusinessMetalAccountsHero/>
     <BusinessMetalAccountsDetails/>
     <ReviewsSwiper/>
+    <FloatingChatWidget/>
     <Footer/>
     </>
 )
