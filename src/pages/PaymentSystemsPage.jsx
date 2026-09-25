b1e002e19d96925541eb74a5584f95bc80923a50
Temur1221 <tem67904@gmail.com>
2026-09-25 20:59:26 +0400

    Update PaymentSystemsPage page

--- src/pages/PaymentSystemsPage.jsx
+++ src/pages/PaymentSystemsPage.jsx
@@ -3,12 +3,15 @@ import MoneyTransfersSubNav from "../MoneyTransfersSubNav";
 import MoneyTransfersSystems from "../MoneyTransfersSystems"; // Այս տողը պարտադիր պետք է լինի
 import Footer from '../Footer';
 import Header from "../Header";
+import FloatingChatWidget from '../FloatingChatWidget'
+
 export default function PaymentSystemsPage() {
   return (
       <>
       <Header/>
       <MoneyTransfersSubNav />
       <MoneyTransfersSystems />
+      <FloatingChatWidget/>
       <Footer/>
   </>
   );
