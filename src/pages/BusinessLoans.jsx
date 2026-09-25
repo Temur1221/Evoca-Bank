299a6e024d870590ab1749630310e284f69956e9
Temur1221 <tem67904@gmail.com>
2026-09-25 20:55:38 +0400

    Update BusinessLoans page

--- src/pages/BusinessLoans.jsx
+++ src/pages/BusinessLoans.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import BusinessLoansList from '../BusinessLoansList'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function BusinessLoans() {
   return (
     <>
         <Header/>
         <BusinessLoansList/>
+        <FloatingChatWidget/>
         <Footer/>
     </>
   )
