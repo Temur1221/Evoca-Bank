df20c249d4933e60cdd775d49472855517d7bce6
Temur1221 <tem67904@gmail.com>
2026-09-25 21:08:09 +0400

    Add SakyagnerPage page

---
+++ src/pages/SakyagnerPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import FloatingChatWidget from '../FloatingChatWidget'
+
+
+export default function SakyagnerPage() {
+  return (
+    <>
+    <Header/>
+    <FloatingChatWidget/>
+    <Footer/>
+    </>
+  )
+}
