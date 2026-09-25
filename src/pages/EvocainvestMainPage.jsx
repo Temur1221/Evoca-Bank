44d32121db7d06109b760be147e6f24cc1430615
Temur1221 <tem67904@gmail.com>
2026-09-25 20:57:30 +0400

    Update EvocainvestMainPage page

--- src/pages/EvocainvestMainPage.jsx
+++ src/pages/EvocainvestMainPage.jsx
@@ -4,6 +4,7 @@ import SecuritiesSubNav from '../SecuritiesSubNav'
 import Footer from '../Footer'
 import EvocainvestHero from '../EvocainvestHero'
 import EvocainvestDetails from '../EvocainvestDetails'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function EvocainvestMainPage() {
   return (
@@ -12,6 +13,7 @@ export default function EvocainvestMainPage() {
     <SecuritiesSubNav/>
     <EvocainvestHero/>
     <EvocainvestDetails/>
+    <FloatingChatWidget/>
     <Footer/>
     </>
 )
