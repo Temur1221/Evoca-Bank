3ac42349e395a81d5b78fd4a1c5e3f68311519ba
Temur1221 <tem67904@gmail.com>
2026-09-25 21:07:01 +0400

    Add HashvetvutyunnerPage page

---
+++ src/pages/HashvetvutyunnerPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReportsSubNav from '../ReportsSubNav';
+import AuditPage from './AuditPage';
+
+export default function HashvetvutyunnerPage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+        <AuditPage/>
+    </div>
+  );
+}
