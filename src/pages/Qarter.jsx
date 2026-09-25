10b562b6299a881ade733fd0ca3f0f6bead79e4b
Temur1221 <tem67904@gmail.com>
2026-09-25 20:59:40 +0400

    Update Qarter page

--- src/pages/Qarter.jsx
+++ src/pages/Qarter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CardsList from '../CardsList'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function Qarter() {
   return (
     <>
         <CardsList/>
+        <FloatingChatWidget/>
     </>
     )
 }
