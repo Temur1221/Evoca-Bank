3416290771b6e1e7e3d86e95b4add8b7c1965b04
Temur1221 <tem67904@gmail.com>
2026-09-25 20:55:03 +0400

    Update BusinessDeposits page

--- src/pages/BusinessDeposits.jsx
+++ src/pages/BusinessDeposits.jsx
@@ -6,6 +6,7 @@ import BusinessAccountIntroSection from '../BusinessAccountIntroSection'
 import BusinessDepositsDetailsSection from '../BusinessDepositsDetailsSection'
 import BusinessDepositsAccordion from '../BusinessDepositsAccordion'
 import BusinessSubHeader from '../BusinessSubHeader'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function BusinessDeposits() {
   return (
@@ -16,6 +17,7 @@ export default function BusinessDeposits() {
     <BusinessDepositsDetailsSection/>
     <BusinessDepositsAccordion/>
     <EvocaBanner/>
+    <FloatingChatWidget/>
     <Footer/>
     </>
 )
