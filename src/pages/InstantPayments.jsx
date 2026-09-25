50d0ec96754313882d519c63a73d878b366ff939
Temur1221 <tem67904@gmail.com>
2026-09-25 20:58:15 +0400

    Update InstantPayments page

--- src/pages/InstantPayments.jsx
+++ src/pages/InstantPayments.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import InstantPaymentHeader from '../InstantPaymentHeader'
 import MainCategories from '../MainCategories'
 import SiteFooter from '../SiteFooter'
+import FloatingChatWidget from '../FloatingChatWidget'
 
 export default function InstantPayments() {
   return (
     <>
         <InstantPaymentHeader/>
         <MainCategories/>
+        <FloatingChatWidget/>
         <SiteFooter/>
     </>
 )
